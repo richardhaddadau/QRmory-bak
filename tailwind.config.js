b4966f5b9229c965f6c3058a12a45bf876fe4c4d
Richard Haddad <richard@haddads.net.au>
2022-08-22 12:28:54 +1000

    build: hot pink gradations

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,15 @@ module.exports = {
                 sans: ["Nunito", ...defaultTheme.fontFamily.sans],
             },
             colors: {
-                "hot-pink": "#ef476f",
+                "hot-pink-100": "#FDD8D3",
+                "hot-pink-200": "#FBABA9",
+                "hot-pink-300": "#F47C84",
+                "hot-pink-400": "#E95A72",
+                "hot-pink-500": "#DB2857",
+                "hot-pink-600": "#BC1D56",
+                "hot-pink-700": "#9D1452",
+                "hot-pink-800": "#7F0C4B",
+                "hot-pink-900": "#690747",
             },
             height: {
                 "main-card": "584px",
