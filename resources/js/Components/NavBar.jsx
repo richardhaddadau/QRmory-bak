dbaaf53b0b0fb3ffceb8edfab06d67a61f13da0e
Richard Haddad <richard@haddads.net.au>
2022-10-17 19:40:43 +1000

    build: dashboard setup

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -3,12 +3,14 @@ import React, { useEffect } from "react";
 import Navigation from "@/Components/Navigation";
 import Logo from "@/Components/Logo";
 import NavBarAction from "@/Components/NavBarAction";
+import ShortLogo from "@/Components/ShortLogo";
 
 const NavBar = ({
     props,
     className = "",
     absolute = true,
     logoColour = "white",
+    fullLogo = true,
 }) => {
     const navClasses =
         className +
@@ -16,23 +18,39 @@ const NavBar = ({
         (absolute ? "absolute" : "relative") +
         " py-4 px-8 top-0 flex flex-col items-center min-h-16 w-full z-50";
 
-    // useEffect(() => {
-    //
-    // }, []);
-
     return (
         <>
             <nav className={navClasses}>
                 <div className="grid grid-cols-12 col-span-12 w-full max-w-7xl">
                     <section className="flex flex-row items-center col-span-3">
                         <a href="/">
-                            <Logo className="w-28" logoColour={logoColour} />
+                            {fullLogo ? (
+                                <Logo
+                                    className="w-28"
+                                    logoColour={logoColour}
+                                />
+                            ) : (
+                                <ShortLogo
+                                    className="w-12"
+                                    logoColour={logoColour}
+                                />
+                            )}
                         </a>
                     </section>
                     <section className="flex flex-row items-center col-span-6">
                         <Navigation />
                     </section>
                     <section className="flex flex-row items-center justify-end col-span-3">
+                        <NavBarAction
+                            value="Dashboard"
+                            destination={route("dashboard")}
+                        />
+
+                        <NavBarAction
+                            value="Logout"
+                            destination={route("dashboard")}
+                        />
+
                         {props.auth.user ? (
                             <NavBarAction
                                 value="Dashboard"
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import Authenticated from '@/Layouts/Authenticated';
-import { Head } from '@inertiajs/inertia-react';
+import React from "react";
+import Authenticated from "@/Layouts/Authenticated";
+import { Head } from "@inertiajs/inertia-react";
+import Standard from "@/Layouts/Standard";
+import NavBar from "@/Components/NavBar";
+import DashNavBar from "@/Components/DashNavBar";
 
 export default function Dashboard(props) {
     return (
-        <Authenticated
-            auth={props.auth}
-            errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
+        // <Authenticated
+        //     auth={props.auth}
+        //     errors={props.errors}
+        //     header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        // >
+        //     <Head title="Dashboard" />
+        //
+        //     <div className="py-12">
+        //         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        //             <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        //                 <div className="p-6 bg-white border-b border-gray-200">You're logged in!</div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </Authenticated>
+        <>
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
-        </Authenticated>
+            <Standard>
+                <DashNavBar props={props} />
+
+                <main className="mx-auto px-6 w-full max-w-7xl">
+                    <section className="mx-auto py-24 text-center text-qrmory-purple-500">
+                        asdasd
+                    </section>
+                </main>
+            </Standard>
+        </>
     );
 }
