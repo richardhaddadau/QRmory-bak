60117f60684ed0844fb2409199bf4c4f50c047a4
Richard Haddad <richard@haddads.net.au>
2022-10-22 23:46:13 +1000

    config: added auth to dashboard route

--- routes/web.php
+++ routes/web.php
@@ -84,10 +84,6 @@ Route::get('/cookie-policy', function () {
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->name('dashboard');
-
-//Route::get('/dashboard', function () {
-//    return Inertia::render('Dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
